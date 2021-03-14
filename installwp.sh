db9512c6d1f045c2bdf13543488166e716e20c2b
Mabrook5 <46682883+Mabrook5@users.noreply.github.com>
2021-03-14 17:49:04 +0100

    Add files via upload

---
+++ installwp.sh
@@ -0,0 +1,11 @@
+#\!/bin/bash;
+
+sudo docker exec -it wordpress apt-get update;
+
+sudo docker exec -it wordpress curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar;
+
+sudo docker exec -it wordpress chmod +x wp-cli.phar;
+
+sudo docker exec -it wordpress mv wp-cli.phar /usr/local/bin/wp;
+
+sudo docker exec -it wordpress wp core install --url=URL_DU_SITE --title="P10AICBG" --admin_user=wpuser --admin_password=123 --admin_email=info@example.com --allow-root;
